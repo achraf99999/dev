d9225b878d367c17bbd25cb684551ba25526ca97
achraf99999 <achref.lassoued@essai.ucar.tn>
2024-09-26 23:03:27 +0100

    setting change

--- image_noise_project/image_noise_project12/settings.py
+++ image_noise_project/image_noise_project12/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'image_noise_project.wsgi.application'
+WSGI_APPLICATION = 'image_noise_project12.wsgi.application'
 
 
 # Database
